832851a6f1f3b25ad355977260d30f7b235711e3
Matongo2010 <justineadriano638@gmail.com>
2019-08-06 14:44:39 +0300

    manage.py

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-export SECRET_KEY='meroka'
-export MAIL_USERNAME='justineadriano638@gmail.com'
-export MAIL_PASSWORD='matongome9291'
-
-
+export MAIL_USERNAME ='risperakinyi3@gmail.com'
+export   MAIL_PASSWORD ='november3'
+export SECRET_KEY='ras'
+SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 python3.6 manage.py server
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringaschool/pitchZone/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Downloads/PitchBlog/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
